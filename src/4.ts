e133aec99feb457735837aef868e1409c742b110
YN <julia.nikit.89@gmail.com>
2026-05-04 19:58:15 +0200

    clean code

--- src/4.ts
+++ src/4.ts
@@ -1,15 +1,3 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export { };
-
 class Key {
   private signature: number;
 
